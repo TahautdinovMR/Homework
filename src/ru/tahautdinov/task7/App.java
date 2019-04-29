548f8ffe3406fd1b4d175fec39d672e0355bd603
TahautdinovMR <tahautdinov@inbox.ru>
2019-04-29 22:35:40 +0300

    zadanie7izm

--- src/ru/tahautdinov/task7/App.java
+++ src/ru/tahautdinov/task7/App.java
@@ -22,45 +22,17 @@ public class App {
 
         int pay = scan.nextInt();
         int sum = pay;
-        if (button == 1) {
-            while (sum < menu[0].price) {
+            while (sum < menu[button - 1].price) {
                 pay = scan.nextInt();
                 sum = sum + pay;
             }
             System.out.println("Ваш напиток готов");
-            if (sum > menu[0].price)
-                System.out.println("Сдача составляет   " + (sum - menu[0].price));
-        }
+            if (sum > menu[button - 1].price)
+                System.out.println("Сдача составляет   " + (sum - menu[button - 1].price));
 
-        if (button == 2) {
-            while (sum < menu[1].price) {
-                pay = scan.nextInt();
-                sum = sum + pay;
-            }
-            System.out.println("Ваш напиток готов");
-            if (sum > menu[1].price)
-            System.out.println("Сдача составляет   " + (sum - menu[1].price));
-        }
-        if (button == 3) {
-            while (sum < menu[2].price) {
-                pay = scan.nextInt();
-                sum = sum + pay;
-            }
-            System.out.println("Ваш напиток готов");
-            if (sum > menu[2].price)
-                System.out.println("Сдача составляет   " + (sum - menu[2].price));
-        }
-        if (button == 4) {
-            while (sum < menu[3].price) {
-                pay = scan.nextInt();
-                sum = sum + pay;
-            }
-            System.out.println("Ваш напиток готов");
-            if (sum > menu[3].price)
-                System.out.println("Сдача составляет   " + (sum - menu[3].price));
         }
     }
-}
+
 
 
 
